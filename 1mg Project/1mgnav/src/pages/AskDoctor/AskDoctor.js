2239cb366b657562fe9ba5aea1d383610bae9269
Anand Kumar Gupta <101396215+Anandgupta09070@users.noreply.github.com>
2022-09-29 18:41:44 +0530

    Nav file Added

---
+++ 1mg Project/1mgnav/src/pages/AskDoctor/AskDoctor.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const AskDoctor = () => {
+  return (
+    <div>AskDoctor</div>
+  )
+}
+
+export default AskDoctor
---
+++ 1mg Project/1mgnav/src/pages/Ayurveda/Ayurveda.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const Ayurveda = () => {
+  return (
+    <div>Ayurveda</div>
+  )
+}
+
+export default Ayurveda
